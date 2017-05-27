c04f6f4e07b66eb7d285c340d490a8009f554587
aaccurso <accurso.alan@gmail.com>
2017-05-27 20:38:00 -0300

    Add eslint configuration
    Also add precommit hook to lint staged files.

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    extends: ['valtech'],
+    env: {
+        node: true,
+        es6: true
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = {
-  extends: ['stylelint-config-standard']
+    extends: ['stylelint-config-standard']
 };
