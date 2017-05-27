1e139e01ac5ff4ff571df6aaa2d024c3b3a5d81a
aaccurso <accurso.alan@gmail.com>
2017-05-27 20:52:08 -0300

    Add Samsung project's rules

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-    extends: ['stylelint-config-standard']
+    extends: ['stylelint-config-standard'],
+    rules: {
+        // Prefer .5 over 0.5 notation for brevity
+        'number-leading-zero': 'never',
+        // Vendor prefixes should be handled during the build process
+        'property-no-vendor-prefix': true,
+        // Reasons for disabling this rule in the code should be documented inline
+        'declaration-no-important': true
+    }
 };
